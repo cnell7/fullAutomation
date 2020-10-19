bf8476ecf8571531cd2c8e65fac72fed0e954eb2
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-10-19 09:38:31 -0400

    added loadSTL

--- fullAutomation.py
+++ fullAutomation.py
@@ -3,13 +3,19 @@ import vtk
 
 
 def matchBrainTags():
+    brainDir = "./Priority2SaveDir"
+    keyChainDir = "./Priority2NameTags"
+    testFile = "/mnt/c/Users/Christian/Code/research/niral/fullautomation/Priority2SaveDir/stx_neo-0078-1-6year_mid_keyChain.stl"
     matchedBrainTags = {}
-    brainScans = os.listdir("./Priority2SaveDir")
-    for filename in os.listdir("./Priority2NameTags"):
+    brainScans = os.listdir(brainDir)
+    # Matches the two if the brain filename contains the tag filename
+    for filename in os.listdir(keyChainDir):
         for brain in brainScans:
             if(filename[:len(filename)-4] in brain):
-                matchedBrainTags[filename] = brain
-    
+                matchedBrainTags[filename[:len(filename)-4]] = brain
+
+    transform = vtk.vtkTransform()
+    polyData = loadSTL(testFile)
     return False
 
 
@@ -17,7 +23,17 @@ def createScene():
     return False
 
 
+def loadSTL(name):
+    # Load the given STL file, and return a vtkPolyData object for it
+    reader = vtk.vtkSTLReader()
+    reader.SetFileName(name)
+    reader.Update()
+    polydata = reader.GetOutput()
+    return polydata
+
+
 def main():
+    # Match brain images with their respective name tags
     matchedBrainTags = matchBrainTags()
     return False
 
