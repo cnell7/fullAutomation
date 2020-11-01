706c2d1d5f8ad74e84f3ab73f8b0bdb7f71c90c7
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-01 12:28:24 -0500

    Update fullAutomation.py

--- fullAutomation.py
+++ fullAutomation.py
@@ -3,106 +3,116 @@ import vtk
 import itertools
 
 def main():
+
     # Match keychains and nametags (will be used later when rest of script is working)
-    brainDir = "./Priority2SaveDir"
-    keyChainDir = "./Priority2NameTags"
+    brainDir = "./Priority3SaveDir/"
+    nametagDir = "./Priority3NameTags/"
     matchedBrainTags = {}
     brainScans = os.listdir(brainDir)
     # Matches the two if the brain filename contains the tag filename
-    for filename in os.listdir(keyChainDir):
+    for filename in os.listdir(nametagDir):
         for brain in brainScans:
             if(filename[:len(filename)-4] in brain):
-                matchedBrainTags[filename[:len(filename)-4]] = brain
-
-    # Test keychain input
-    input = ['./Priority2SaveDir/stx_neo-0078-1-6year_mid_keyChain.stl',
-    './Priority2SaveDir/stx_neo-0100-1-6year_mid_keyChain.stl',
-    './Priority2SaveDir/stx_neo-0123-1-6year_mid_keyChain.stl',
-    './Priority2SaveDir/stx_neo-0125-1-6year_mid_keyChain.stl',
-    './Priority2SaveDir/stx_neo-0217-1-6year_mid_keyChain.stl',
-    './Priority2SaveDir/stx_neo-0343-1-1-6year_mid_keyChain.stl']
-
-    # Test nametag input
-    inputTags = ['./Priority2NameTags/0078-1.stl',
-    './Priority2NameTags/0100-1.stl',
-    './Priority2NameTags/0123-1.stl',
-    './Priority2NameTags/0125-1.stl',
-    './Priority2NameTags/0217-1.stl',
-    './Priority2NameTags/0343-1-1.stl']
+                matchedBrainTags[nametagDir + filename] = brainDir + brain
 
     # Hard cooded translation for keychains
-    switcherBrain = {1: [-50,60,0],
-    2: [0,60,0],
-    3: [50,60,0],
-    4: [-50,0,0],
-    5: [0,0,0],
-    6: [50,0,0]}
+    switcherBrain = {0: [-50,60,0],
+    1: [0,60,0],
+    2: [50,60,0],
+    3: [-50,0,0],
+    4: [0,0,0],
+    5: [50,0,0]}
     # Hard cooded translation for nametags
-    switcherNameTag = {1: [-90,60,0],
-    2: [-40,60,0],
-    3: [10,60,0],
-    4: [-90,0,0],
-    5: [-40,0,0],
-    6: [10,0,0]}
+    switcherNameTag = {0: [-90,60,0],
+    1: [-40,60,0],
+    2: [10,60,0],
+    3: [-90,0,0],
+    4: [-40,0,0],
+    5: [10,0,0]}
 
-    keychainBounds = []
-    keychainZMax = 0
+    sceneCount = len(matchedBrainTags) % 6
+    sceneIterator = 0
+    totalBrainLength = len(matchedBrainTags)
+    print(totalBrainLength)
+    macthedKeys = list(matchedBrainTags.keys())
 
-    # Find max z height so that it can be used to raise other surfaces to that level (make level scene)
-    for keychain in input:
-        reader = vtk.vtkSTLReader()  
-        reader.SetFileName(keychain)
+    while sceneIterator < sceneCount:
 
-        mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInputConnection(reader.GetOutputPort())
-        mapper.Update()
-        bounds = mapper.GetBounds()
-        if(bounds[4] > keychainZMax):
-            keychainZMax = bounds[4]
+        keychainBounds = []
+        keychainZMax = 0
 
-    print(keychainZMax)
-    coordinateCount = 1 # Used to keep track of which translation needs to be done for what part of the scene the loop is on.
-    appendFilter = vtk.vtkAppendPolyData()
+        zBoundCounter = 0
+        # Find max z height so that it can be used to raise other surfaces to that level (make level scene)
+        while zBoundCounter < 6:
+            reader = vtk.vtkSTLReader()  
+            reader.SetFileName(matchedBrainTags[macthedKeys[sceneIterator + zBoundCounter]])
 
-    for (i, iT) in zip(input, inputTags):
-        # Append key chain
-        reader = vtk.vtkSTLReader()  # Read keychain
-        reader.SetFileName(i)
+            mapper = vtk.vtkPolyDataMapper()
+            mapper.SetInputConnection(reader.GetOutputPort())
+            mapper.Update()
+            bounds = mapper.GetBounds()
+            if(bounds[4] > keychainZMax):
+                keychainZMax = bounds[4]
+            zBoundCounter += 1
 
-        # Use mapper to get keychain bounds to correctly place in scene
-        mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInputConnection(reader.GetOutputPort())
-        mapper.Update()
-        bounds = mapper.GetBounds() #Return bounding box (array of six doubles) of data expressed as (xmin,xmax, ymin,ymax, zmin,zmax).
+        coordinateCount = 0 # Used to keep track of which translation needs to be done for what part of the scene the loop is on.
+        appendFilter = vtk.vtkAppendPolyData()
 
-        trans = vtk.vtkTransform()   # Set translation
-        coordinates = switcherBrain[coordinateCount]
-        trans.Translate(coordinates[0], coordinates[1], (coordinates[2] + (keychainZMax - bounds[4])))  # Add z offset to final translation
+        appendingCounter = 0
+        while appendingCounter < 6:
+            # Append key chain
+            reader = vtk.vtkSTLReader()  # Read keychain
+            print(matchedBrainTags[macthedKeys[sceneIterator + appendingCounter]])
+            reader.SetFileName(matchedBrainTags[macthedKeys[sceneIterator + appendingCounter]])
 
-        process = vtk.vtkTransformPolyDataFilter()  # Process translate
-        process.SetTransform(trans)
-        process.SetInputConnection(reader.GetOutputPort())
-        process.Update()
+            # Use mapper to get keychain bounds to correctly place in scene
+            mapper = vtk.vtkPolyDataMapper()
+            mapper.SetInputConnection(reader.GetOutputPort())
+            mapper.Update()
+            bounds = mapper.GetBounds() #Return bounding box (array of six doubles) of data expressed as (xmin,xmax, ymin,ymax, zmin,zmax).
 
-        appendFilter.AddInputConnection(process.GetOutputPort())    # Append to scene
-        appendFilter.Update()
+            trans = vtk.vtkTransform()   # Set translation
+            coordinates = switcherBrain[coordinateCount]
+            trans.Translate(coordinates[0], coordinates[1], (coordinates[2] + (keychainZMax - bounds[4])))  # Add z offset to final translation
 
-        # Append name tag
-        reader = vtk.vtkSTLReader() # Read nametag
-        reader.SetFileName(iT)
+            process = vtk.vtkTransformPolyDataFilter()  # Process translate
+            process.SetTransform(trans)
+            process.SetInputConnection(reader.GetOutputPort())
+            process.Update()
 
-        trans = vtk.vtkTransform()  # Set transltion
-        coordinates = switcherNameTag[coordinateCount]
-        trans.Translate(coordinates[0], coordinates[1], coordinates[2] + keychainZMax)
+            appendFilter.AddInputConnection(process.GetOutputPort())    # Append to scene
+            appendFilter.Update()
 
-        process = vtk.vtkTransformPolyDataFilter()  # Process translate
-        process.SetTransform(trans)
-        process.SetInputConnection(reader.GetOutputPort())
-        process.Update()
+            # Append name tag
+            reader = vtk.vtkSTLReader() # Read nametag
+            print(macthedKeys[sceneIterator + appendingCounter])
+            reader.SetFileName(macthedKeys[sceneIterator + appendingCounter])
 
-        appendFilter.AddInputConnection(process.GetOutputPort())    # Append to scene
-        appendFilter.Update()
-        coordinateCount += 1
+            trans = vtk.vtkTransform()  # Set transltion
+            coordinates = switcherNameTag[coordinateCount]
+            trans.Translate(coordinates[0], coordinates[1], coordinates[2] + keychainZMax)
+
+            process = vtk.vtkTransformPolyDataFilter()  # Process translate
+            process.SetTransform(trans)
+            process.SetInputConnection(reader.GetOutputPort())
+            process.Update()
+
+            appendFilter.AddInputConnection(process.GetOutputPort())    # Append to scene
+            appendFilter.Update()
+
+
+            coordinateCount += 1
+            appendingCounter += 1
+            if((coordinateCount + (sceneIterator * 6)) == totalBrainLength):
+                appendingCounter = 7    # End loop because out of surfaces but less than scene max
+
+        # Write print scene
+        writer = vtk.vtkSTLWriter()
+        writer.SetFileName('keyChainScene' + str(sceneIterator) + '.stl')
+        writer.SetInputConnection(appendFilter.GetOutputPort())
+        writer.Write()
+
+        sceneIterator += 1
 
     '''
     # Create test visual of what the scene looks like
@@ -124,11 +134,6 @@ def main():
     renderWindow.Render()
     renderWindowInteractor.Start()
     '''
-    # Write print scene
-    writer = vtk.vtkSTLWriter()
-    writer.SetFileName('./Priority2SaveDir/stx_neo-0078-1-6year_mid_keyChain_2.stl')
-    writer.SetInputConnection(appendFilter.GetOutputPort())
-    writer.Write()
     return True
 
 
